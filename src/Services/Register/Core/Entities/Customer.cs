976ceac72dc6401cf9801c7ba1beec07ef10d3ff
Jadeilson Martiho <jadeilsonm17@gmail.com>
2024-07-05 00:03:27 -0300

    [FEAT] - Add entities and interfaces

--- src/Services/Register/Core/Entities/Customer.cs
+++ src/Services/Register/Core/Entities/Customer.cs
@@ -1,6 +1,23 @@
-﻿namespace Core.Entities;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
-public class Customer
+namespace Core.Entities;
+
+[Table("customer")]
+public class Customer(
+    string name,
+    string email,
+    string cellNumber,
+    string password,
+    DateTime dateOfBirth,
+    string document,
+    Guid id)
 {
-    public Guid Id { get; set; }
+    public string Name { get; set; } = name;
+    public string Email { get; set; } = email;
+    public string CellNumber { get; set; } = cellNumber;
+    public string Password { get; set; } = password;
+    public DateTime DateOfBirth { get; set; } = dateOfBirth;
+    public string Document { get; set; } = document;
+    public Guid Id { get; set; } = id;
 }
+    
--- src/Services/Register/Core/Interfaces/ICustomerRepository.cs
+++ src/Services/Register/Core/Interfaces/ICustomerRepository.cs
@@ -5,6 +5,7 @@ namespace Core.Interfaces;
 public interface ICustomerRepository
 {
     Task<Customer?> GetByIdAsync(Guid id);
+    Task<Boolean> IsExistCustomerWithEmail(string email);
     Task<IEnumerable<Customer>> GetAllAsync();
     Task<Customer> AddCustomerAsync(Customer customer);
 }
